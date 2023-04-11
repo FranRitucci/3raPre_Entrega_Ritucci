8f313a725d2a3cb5f1034c538aac64b9df029ec7
Fran Ritucci <fritucci1037@gmail.com>
2023-04-11 19:25:46 -0300

    agregue un while para asegurar el ingreso de la variable correcta para acceder al switch

--- main.js
+++ main.js
@@ -1,26 +1,32 @@
 let nombreUsuario = (prompt("Ingrese su nombre"));
 let queDeseaHacer = (prompt("Ingrese: extraccion o deposito"));
 
+while (queDeseaHacer != "extraccion" && queDeseaHacer != "deposito"){
+    alert ("La operacion que sedea no se reconoce, ingrese 'extraccion' o ingrese 'deposito'");
+    queDeseaHacer = prompt ("Ingrese: extraccion o deposito");
+}
+
 switch (queDeseaHacer){
     case "extraccion":
         alert (nombreUsuario  + ", puedes extraer hasta $10 000");
         let retiro = Number (prompt("Ingrese el monto a retirar:"));
         if (retiro <=10000){
-            alert("¡Exitoso! El dinero ya esta disponible en su billetera virtual");
+            alert("¡Exitoso! El dinero ya esta disponible en su billetera virtual.");
         } else{
-            alert ("El limite de retiro diario es $10,000. Ingrese ese importe o menos")
+            alert ("El limite de retiro diario es $10,000. No ingrese un monto superior.")
         }
         break;
 
-    case "EXTRACCION":
-        alert ("Puedes extraer hasta $10 000");
-        break;
-
     case "deposito":
-        alert ("Puedes depositar hasta $20 000");
+        alert (nombreUsuario + ", puedes depositar hasta $20 000");
+        let depositar = Number (prompt("Ingrese el monto a depositar:"));
+        if (depositar <=20000){
+            alert ("¡Exitoso! El dinero se acreditara en los proximos minutos en tu billetera virtual.")
+        } else {
+            alert ("El limite de deposito diario es $20,000. No ingrese un monto superior.")
+        }
         break;
 
-    case "DEPOSITO":
-        alert ("Puedes depositar hasta $20 000");
-        break;
+    default:
+        alert ("La operacion que sedea no se reconoce, ingrese 'extraccion' o ingrese 'deposito'");
 }
