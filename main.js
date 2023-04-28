8b4b10e0c96a62ccebf76f104826e9969f1b26bc
Fran Ritucci <fritucci1037@gmail.com>
2023-04-28 13:57:46 -0300

    -en rama copia- se desarrollo un saldo en cuenta que incrementa o decrementa mientras el user deposita y extrae y al mismo tiempo valida la posibilidad de extraccion o no

--- main.js
+++ main.js
@@ -1,6 +1,18 @@
 let nombreUsuario = (prompt("Ingrese su nombre"));
 let queDeseaHacer = (prompt(nombreUsuario + ", para operar ingrese: 'deposito' o 'extraccion'. Para finalizar: 'salir'"));
 
+let saldoEnCuenta = 0;
+
+function sumar(parametro1, parametro2){
+    let saldoEnCuenta = parametro1 + parametro2;
+    return saldoEnCuenta;
+}
+
+function restar(montoDisponible, montoExtraido){
+    saldoEnCuenta = montoDisponible - montoExtraido;
+    return saldoEnCuenta;
+}
+
 while (queDeseaHacer != "salir") {
     while (queDeseaHacer != "extraccion" && queDeseaHacer != "deposito") {
         alert("La operacion que sedea no se reconoce, ingrese 'deposito' o ingrese 'extraccion'");
@@ -12,28 +24,44 @@ while (queDeseaHacer != "salir") {
             alert(nombreUsuario + ", puedes depositar un monto desde $5.000 hasta $20.000");
             let depositar = Number(prompt(nombreUsuario + ", ingrese el monto a depositar:"));
             if (depositar <= 20000 && depositar >= 5000) {
-                dineroAcreditado();
+                saldoEnCuenta = sumar(saldoEnCuenta, depositar);
+                alert(nombreUsuario + ", su saldo en cuenta es: $" + saldoEnCuenta + " y ya se encuentra acreditado.");
                 saludoAgradecimiento();
             } else {
                 while (depositar > 20000 || depositar < 5000) {
                     depositar = Number(prompt(nombreUsuario + ", puede depositar minimamente $5.000 y maximo $20.000"));
                 }
-                dineroAcreditado();
+                saldoEnCuenta = sumar(saldoEnCuenta, depositar);
+                alert("Su saldo en cuenta es: " + saldoEnCuenta);
                 saludoAgradecimiento();
             }
             break;
 
         case "extraccion":
-            alert(nombreUsuario + ", puedes extraer un monto desde $1.000 hasta $10.000");
-            let retiro = Number(prompt(nombreUsuario + ", ingrese el monto a retirar:"));
-            if (retiro <= 10000 && retiro >= 1000) {
-                dineroExtraido();
-                saludoAgradecimiento();
-            } else {
-                while (retiro > 10000 || retiro < 1000) {
-                    retiro = Number(prompt(nombreUsuario + ", puede retirar minimamente $1.000 y maximo $10.000"));
+            if (saldoEnCuenta < 1000) {
+                alert(nombreUsuario + ", el saldo disponible en cuenta es menor al minimo de retiro. Su saldo en cuenta es: $" + saldoEnCuenta + ". Ingrese ' cancelar ' para volver al menu");
+                let cancelarOperacion = (prompt("Ingrese 'cancelar' para cancelar y seguir operando."));
+                while (cancelarOperacion != "cancelar"){
+                    alert(nombreUsuario + ", el saldo disponible en cuenta es menor al minimo de retiro. Su saldo en cuenta es: $" + saldoEnCuenta + ". Ingrese ' cancelar ' para cancelar");
+                    cancelarOperacion = (prompt("Ingrese 'cancelar' para cancelar y seguir operando."));
                 }
-                dineroExtraido();
+            } else if (saldoEnCuenta >= 1000) {
+                alert(nombreUsuario + ", puedes extraer minimamente $1.000 y tu saldo disponible en cuenta es: $" + saldoEnCuenta);
+                let retiro = Number(prompt(nombreUsuario + ", su saldo disponible es: $ " + saldoEnCuenta + ". ingrese el monto a retirar:"));
+                while (retiro < 1000){
+                    alert(nombreUsuario + ", puedes extraer minimamente $1.000 y tu saldo disponible en cuenta es: $" + saldoEnCuenta);
+                    retiro = Number(prompt(nombreUsuario + ", su saldo disponible es: $" + saldoEnCuenta + "ingrese el monto a retirar:"));
+                }
+                dineroExtraido()
+                saldoEnCuenta = restar(saldoEnCuenta, retiro);
+                alert("Su saldo en cuenta es: $" + saldoEnCuenta);
+                saludoAgradecimiento()
+            } else {
+                while (retiro > saldoEnCuenta) {
+                    retiro = Number(prompt(nombreUsuario + ", puede retirar minimamente $1.000 y su saldo disponible en cuenta que es: $" + saldoEnCuenta));
+                }
+                saldoEnCuenta = restar(saldoEnCuenta, retiro);
+                alert("Su saldo en cuenta es: " + saldoEnCuenta);
                 saludoAgradecimiento();
             }
             break;
@@ -42,11 +70,7 @@ while (queDeseaHacer != "salir") {
 }
 
 function dineroExtraido() {
-    alert("¡Exitoso! El dinero ya esta disponible en tu billetera virtual.")
-}
-
-function dineroAcreditado() {
-    alert("¡Exitoso! El dinero se acreditara en los proximos minutos en tu cuenta.")
+    alert("¡Exitoso! El dinero se ha extraido hacia tu billetera virtual.")
 }
 
 function saludoAgradecimiento() {
