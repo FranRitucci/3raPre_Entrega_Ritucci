1fbbd1d31137a6c9656896cd2eec5ea2f5451d54
Fran Ritucci <fritucci1037@gmail.com>
2023-04-29 20:33:54 -0300

    se agrego un objeto e interaccion con el

--- main.js
+++ main.js
@@ -1,22 +1,24 @@
 let nombreUsuario = (prompt("Ingrese su nombre"));
-let queDeseaHacer = (prompt(nombreUsuario + ", para operar ingrese: 'deposito' o 'extraccion'. Para finalizar: 'salir'"));
+let apellidoUsuario = (prompt("Ingrese su apellido"));
+let suEdad = (prompt("Ingrese su edad"));
+let queDeseaHacer = (prompt(nombreUsuario + ", para operar ingrese: 'deposito', 'extraccion' o ' mi perfil '. Para finalizar: 'salir'"));
 
 let saldoEnCuenta = 0;
 
-function sumar(parametro1, parametro2){
+function sumar(parametro1, parametro2) {
     let saldoEnCuenta = parametro1 + parametro2;
     return saldoEnCuenta;
 }
 
-function restar(montoDisponible, montoExtraido){
+function restar(montoDisponible, montoExtraido) {
     saldoEnCuenta = montoDisponible - montoExtraido;
     return saldoEnCuenta;
 }
 
 while (queDeseaHacer != "salir") {
-    while (queDeseaHacer != "extraccion" && queDeseaHacer != "deposito") {
-        alert("La operacion que sedea no se reconoce, ingrese 'deposito' o ingrese 'extraccion'");
-        queDeseaHacer = prompt("Ingrese: deposito o extraccion");
+    while (queDeseaHacer != "extraccion" && queDeseaHacer != "deposito" && queDeseaHacer != "mi perfil") {
+        alert("La operacion que sedea no se reconoce, ingrese 'deposito', 'extraccion' o ingrese 'mi perfil'");
+        queDeseaHacer = prompt("Ingrese: deposito, extraccion o mi perfil");
     }
 
     switch (queDeseaHacer) {
@@ -41,14 +43,14 @@ while (queDeseaHacer != "salir") {
             if (saldoEnCuenta < 1000) {
                 alert(nombreUsuario + ", el saldo disponible en cuenta es menor al minimo de retiro. Su saldo en cuenta es: $" + saldoEnCuenta + ". Ingrese ' cancelar ' para volver al menu");
                 let cancelarOperacion = (prompt("Ingrese 'cancelar' para cancelar y seguir operando."));
-                while (cancelarOperacion != "cancelar"){
+                while (cancelarOperacion != "cancelar") {
                     alert(nombreUsuario + ", el saldo disponible en cuenta es menor al minimo de retiro. Su saldo en cuenta es: $" + saldoEnCuenta + ". Ingrese ' cancelar ' para cancelar");
                     cancelarOperacion = (prompt("Ingrese 'cancelar' para cancelar y seguir operando."));
                 }
             } else if (saldoEnCuenta >= 1000) {
                 alert(nombreUsuario + ", puedes extraer minimamente $1.000 y tu saldo disponible en cuenta es: $" + saldoEnCuenta);
-                let retiro = Number(prompt(nombreUsuario + ", su saldo disponible es: $ " + saldoEnCuenta + ". ingrese el monto a retirar:"));
-                while (retiro < 1000){
+                let retiro = Number(prompt(nombreUsuario + ", su saldo disponible es: $" + saldoEnCuenta + ". ingrese el monto a retirar:"));
+                while (retiro < 1000) {
                     alert(nombreUsuario + ", puedes extraer minimamente $1.000 y tu saldo disponible en cuenta es: $" + saldoEnCuenta);
                     retiro = Number(prompt(nombreUsuario + ", su saldo disponible es: $" + saldoEnCuenta + "ingrese el monto a retirar:"));
                 }
@@ -65,8 +67,19 @@ while (queDeseaHacer != "salir") {
                 saludoAgradecimiento();
             }
             break;
+
+        case "mi perfil":
+            const miUsuario = {
+                nombre: nombreUsuario,
+                apellido: apellidoUsuario,
+                edad: suEdad,
+                saldo: saldoEnCuenta,
+                versionApp: "Version2.0"
+            }
+            alert("Detalles del usuario>> Nombre: " + miUsuario.nombre + " >Apellido: " + miUsuario.apellido + " >Edad: " + miUsuario.edad + " >Saldo: " + miUsuario.saldo + " >AppVersion: " + miUsuario.versionApp);
+            break;
     }
-    queDeseaHacer = (prompt(nombreUsuario + ", para operar ingrese: 'deposito' o 'extraccion'. Para finalizar: 'salir'"));
+    queDeseaHacer = (prompt(nombreUsuario + ", para operar ingrese: 'deposito', 'extraccion' o 'mi perfil'. Para finalizar: 'salir'"));
 }
 
 function dineroExtraido() {
