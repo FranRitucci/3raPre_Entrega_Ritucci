af1fd27837d8e9a60f8757f5b12a3eb77c3419ad
Fran Ritucci <fritucci1037@gmail.com>
2023-04-30 20:23:39 -0300

    se creo array que contiene condiciones de validacion

--- main.js
+++ main.js
@@ -1,8 +1,42 @@
+//ARRAY QUE CONSERVA LAS CONDICIONES DE SEGURIDAD PARA VALIDAR UN USUARIO HUMANO. PROMPTs CON WHILEs PARA VALIDAR LA RESPUESTA CORRECTA Y PERMITIR EL AVANCE
+const validacionSeguridad = ["2+2 =", "3x3 =", "Escriba: aUtobUs", "Nombre propio de 6 letras:", "El ultimo campeon del mundo:"];
+
+alert("¡Bienvenido a Billetera virtual 2.0! Resuelva las siguientes pautas para comprobar que usted NO es un robot:");
+
+alert(validacionSeguridad[0]);
+let validacion1 = Number(prompt(""));
+while (validacion1 != 4) {
+    validacion1 = (prompt("2+2 ="));
+}
+alert(validacionSeguridad[1]);
+let validacion2 = Number(prompt(""));
+while (validacion2 != 9){
+    validacion2 = Number(prompt("3x3 ="));
+}
+alert(validacionSeguridad[2]);
+let validacion3 = (prompt(""));
+while (validacion3 != "aUtobUs"){
+    validacion3 = (prompt("aUtobUs ="));
+}
+alert(validacionSeguridad[3]);
+let validacion4 = (prompt(""));
+while (validacion4.length !== 6 ||/[^a-zA-Z]/.test(validacion4)){
+    validacion4 = (prompt("Un nombre de 6 letras ="));
+}
+alert(validacionSeguridad[4]);
+let validacion5 = (prompt(""));
+while (validacion5 != "argentina"){
+    validacion5 = (prompt("La mejor seleccion de futbol mundial ="));
+}
+alert("Usted cumplio con la verificacion de seguridad y reconoce que la selccion ARGENTINA es la mas grande del mundo. Puede continuar");
+
+//VARIABLES QUE CONFORMAN EL PERFIL DEL USUARIO Y LA OPERACION CON LA QUE DESEA PROCEDER
 let nombreUsuario = (prompt("Ingrese su nombre"));
 let apellidoUsuario = (prompt("Ingrese su apellido"));
-let suEdad = (prompt("Ingrese su edad"));
+let suEdad = Number(prompt("Ingrese su edad"));
 let queDeseaHacer = (prompt(nombreUsuario + ", para operar ingrese: 'deposito', 'extraccion' o ' mi perfil '. Para finalizar: 'salir'"));
 
+//VARIABLE QUE CONSERVA EL SALDO EN CUENTA. INICIALIZADA EN 0 Y FUNCIONES QUE INCREMENTAN Y DECREMENTAN EL SALDO SEGUN DEPOSITOS Y EXTRACCIONES
 let saldoEnCuenta = 0;
 
 function sumar(parametro1, parametro2) {
@@ -15,6 +49,7 @@ function restar(montoDisponible, montoExtraido) {
     return saldoEnCuenta;
 }
 
+//FUNCIONES PARA PRESENTAR ALERTAS DE CONFIRMACION Y SALUDO PARA EL USUARIO AL FINALIZAR LAS OPERACIONES
 function dineroExtraido() {
     alert("¡Exitoso! El dinero se ha extraido hacia tu billetera virtual.")
 }
@@ -23,6 +58,7 @@ function saludoAgradecimiento() {
     alert("¡Gracias por operar con nuestra Billetera Virtual!")
 }
 
+//NUCLEO DEL PROGRAMA. WHILE QUE MANTIENE EL PROGRAMA EN CICLO - WHILE PARA VALIDAR EL INGRESO DE LAS OPERACIONES CORRECTAS - SWITCH QUE CONTIENE LAS OPERACIONES
 while (queDeseaHacer != "salir") {
     while (queDeseaHacer != "extraccion" && queDeseaHacer != "deposito" && queDeseaHacer != "mi perfil") {
         alert("La operacion que sedea no se reconoce, ingrese 'deposito', 'extraccion' o ingrese 'mi perfil'");
